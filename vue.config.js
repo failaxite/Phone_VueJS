b63f59266f439f089a5448ec7c320acd87e3820e
Felix/failaxite <failaxite.don@gmail.com>
2023-03-29 23:44:47 +0200

    add publicpath

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+
+const publicPath = process.env.NODE_ENV === 'production' ? '/personnage/' : '/'
+module.exports = {
+ publicPath: publicPath,
+}
